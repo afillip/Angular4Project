47ec8be2ef64b66379e4bc27c263d09564078289
Amanda Fillip <amanda@dronesense.com>
2017-09-07 11:33:26 -0500

    added initital components to project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,25 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { RecipesComponent } from './recipes/recipes.component';
+import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
+import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component';
+import { RecipeItemComponent} from './recipes/recipe-list/recipe-item/recipe-item.component';
+import { ShoppingListComponent } from './shoppingList/shoppingList.component';
+import { ShoppingEditComponent } from './shoppingList/shopping-edit/shopping-edit.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    RecipesComponent,
+    RecipeDetailComponent,
+    RecipeListComponent,
+    RecipeItemComponent,
+    ShoppingListComponent,
+    ShoppingEditComponent,
+
   ],
   imports: [
     BrowserModule
