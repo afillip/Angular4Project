b1d0840a077a149a085b5c08c3f475ef84e8aee0
Amanda Fillip <amanda@dronesense.com>
2017-09-07 13:29:13 -0500

    basic recipe and shopping list utlities laid down

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,6 +7,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Array<Recipe> = [
+    new Recipe('A test Recipe', 'This is simply a test.',
+      'https://i.pinimg.com/736x/b4/1d/a4/b41da48878e37f762ff0b1988ed2de82--lunch-recipes-delicious-recipes.jpg')
+  ];
 
   constructor() { }
 
